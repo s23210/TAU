70588b77ec771de294ad330f81f6e951c8315bf4
s23210 <73021331+s23210@users.noreply.github.com>
2023-12-02 15:27:21 +0100

    Add LOGGER

--- LAB2/Selenium/src/test/java/InvestmentFundsSearchTest.java
+++ LAB2/Selenium/src/test/java/InvestmentFundsSearchTest.java
@@ -1,3 +1,5 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,8 +15,11 @@ public class InvestmentFundsSearchTest {
     private String url;
     private String searchPhrase;
 
+    private static final Logger LOGGER = Logger.getLogger(InvestmentFundsSearchTest.class.getName());
+
     @Before
     public void setUp() {
+        LOGGER.info("Setting up the WebDriver and test data.");
         webDriver = new EdgeDriver();
         url = "https://www.bankmillennium.pl/";
         searchPhrase = "fundusz";
@@ -22,18 +27,30 @@ public class InvestmentFundsSearchTest {
 
     @Test
     public void isInvestmentFundRedirectUrlPresentAfterSearch() throws InterruptedException {
+        LOGGER.info("Starting the search test.");
+
         webDriver.get(url);
+        LOGGER.log(Level.INFO, "Navigating to URL: {0}", url);
+
         webDriver.findElement(By.id("search"))
                 .sendKeys(searchPhrase);
+        LOGGER.log(Level.INFO, "Entered search phrase: {0}", searchPhrase);
+
         webDriver.findElement(By.id("search"))
                 .sendKeys(Keys.RETURN);
+        LOGGER.info("Pressed Enter key.");
+
         Thread.sleep(1000);
         Assert.assertTrue(webDriver.findElement(By.xpath("//a[contains(@href,'https://www.bankmillennium.pl/klienci-indywidualni/inwestycje')]"))
                 .isDisplayed());
+        LOGGER.info("Assertion passed: Redirect URL is displayed.");
+
+        LOGGER.info("Search test completed.");
     }
 
     @After
     public void tearDown() {
+        LOGGER.info("Tearing down the WebDriver.");
         webDriver.quit();
     }
 }
--- LAB2/Selenium/src/test/java/MailLoginTest.java
+++ LAB2/Selenium/src/test/java/MailLoginTest.java
@@ -1,9 +1,12 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MailLoginTest {
@@ -12,10 +15,12 @@ public class MailLoginTest {
     private String userName;
     private String userPassword;
 
+    private static final Logger LOGGER = Logger.getLogger(MailLoginTest.class.getName());
 
     @Before
     public void setUp() {
-        webDriver = new FirefoxDriver();
+        LOGGER.info("Setting up the WebDriver and test data.");
+        webDriver = new ChromeDriver();
         url = "https://profil.wp.pl/login/login.html";
         userName = "piotrek.piotr2023@wp.pl";
         userPassword = "1qaz2wsxXSW@ZAQ!";
@@ -23,25 +28,40 @@ public class MailLoginTest {
 
     @Test
     public void testMailLogin() throws InterruptedException {
+        LOGGER.info("Starting the login test.");
+
         webDriver.get(url);
+        LOGGER.log(Level.INFO, "Navigating to URL: {0}", url);
+
         Thread.sleep(1000);
         webDriver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[3]/div/button[2]"))
                 .click();
-        webDriver.
-                findElement(By.id("login"))
+        LOGGER.info("Clicked on the login button with xpath.");
+
+        webDriver.findElement(By.id("login"))
                 .sendKeys(userName);
+        LOGGER.log(Level.INFO, "Entered username: {0}", userName);
+
         webDriver.findElement(By.id("password"))
                 .sendKeys(userPassword);
+        LOGGER.info("Entered password.");
+
         Thread.sleep(1000);
         webDriver.findElement(By.xpath("//button[@type='submit']"))
                 .click();
+        LOGGER.info("Clicked on the submit button.");
+
         Thread.sleep(1000);
         Assert.assertTrue(webDriver.findElement(By.xpath("//*[@id=\"1\"]"))
                 .isDisplayed());
+        LOGGER.info("Assertion passed: Element is displayed.");
+
+        LOGGER.info("Login test completed.");
     }
 
     @After
     public void tearDown() {
+        LOGGER.info("Tearing down the WebDriver.");
         webDriver.quit();
     }
 }
